aeeffce0eb70d42f8a64372c08f28b171ee6e115
mvrigkas <mvrigkas@users.noreply.github.com>
2018-02-21 15:05:08 -0600

    publication updates

--- assets/js/hidebib.js
+++ assets/js/hidebib.js
@@ -11,6 +11,19 @@ function hideallbibs()
     }
 }
 
+function hideallabstracts()
+{
+    var el = document.getElementsByTagName("div") ;
+    for (var i = 0 ; i < el.length ; ++i) {
+        if (el[i].className == "publication-abstract") {
+            var bib = el[i].getElementsByTagName("pre") ;
+            if (bib.length > 0) {
+                bib [0] .style.display = 'none' ;
+            }
+        }
+    }
+}
+
 function togglebib(paperid)
 {
     var paper = document.getElementById(paperid) ;
